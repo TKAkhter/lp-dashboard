66b6101a190f184047f0cd671d9a152ab90aa805
Talha Akhter <talhaakhter01@gmail.com>
2021-06-17 14:45:44 +0500

    libary conflicts resolved

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -7,36 +7,36 @@ window.onload = function () {
   });
 
   // Pie Chart Example
-  var ctx = document.getElementById("myPieChart");
-  var myPieChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-      labels: ["Direct", "Referral", "Social"],
-      datasets: [{
-        data: [55, 30, 15],
-        backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-        hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
-        hoverBorderColor: "rgba(234, 236, 244, 1)",
-      }],
-    },
-    options: {
-      maintainAspectRatio: false,
-      tooltips: {
-        backgroundColor: "rgb(255,255,255)",
-        bodyFontColor: "#858796",
-        borderColor: '#dddfeb',
-        borderWidth: 1,
-        xPadding: 15,
-        yPadding: 15,
-        displayColors: false,
-        caretPadding: 10,
-      },
-      legend: {
-        display: false
-      },
-      cutoutPercentage: 80,
-    },
-  });
+  // var ctx = document.getElementById("myPieChart");
+  // var myPieChart = new Chart(ctx, {
+  //   type: 'doughnut',
+  //   data: {
+  //     labels: ["Direct", "Referral", "Social"],
+  //     datasets: [{
+  //       data: [55, 30, 15],
+  //       backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+  //       hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+  //       hoverBorderColor: "rgba(234, 236, 244, 1)",
+  //     }],
+  //   },
+  //   options: {
+  //     maintainAspectRatio: false,
+  //     tooltips: {
+  //       backgroundColor: "rgb(255,255,255)",
+  //       bodyFontColor: "#858796",
+  //       borderColor: '#dddfeb',
+  //       borderWidth: 1,
+  //       xPadding: 15,
+  //       yPadding: 15,
+  //       displayColors: false,
+  //       caretPadding: 10,
+  //     },
+  //     legend: {
+  //       display: false
+  //     },
+  //     cutoutPercentage: 80,
+  //   },
+  // });
 
   const data = {
     labels: ["Direct", "Referral", "Social"],
@@ -52,60 +52,25 @@ window.onload = function () {
     type: 'pie',
     data: data,
     options: {
-      // circumference: 2 * Math.PI,
-      // rotation: -0.8 * Math.PI,
       animation: {
-        // animateRotate: true,
         animateScale: true
       },
       legend: {
         position: 'bottom',
+        display: true,
         labels: {
-          // fontColor: "white",
           boxWidth: 20,
           padding: 10
         }
       },
-      // responsive: true,
       maintainAspectRatio: false,
-      // scales: {
-      //     yAxes: [{
-      //         ticks: {
-      //             beginAtZero: true
-      //         }
-      //     }]
-      // },
       plugins: {
-        // datalabels: {
-        //   // backgroundColor: function (context) {
-        //   //   return context.dataset.backgroundColor;
-        //   // },
-        //   borderColor: 'white',
-        //   borderRadius: 25,
-        //   borderWidth: 25,
-        //   color: 'white',
-        //   display: function (context) {
-        //     var dataset = context.dataset;
-        //     var count = dataset.data.length;
-        //     var value = dataset.data[context.dataIndex];
-        //     return value > count * 1.5;
-        //   },
-        //   font: {
-        //     weight: 'bold'
-        //   },
-        //   padding: 6,
-        //   formatter: Math.round
-        // },
         datalabels: {
-          render: 'percentage',
-          position: 'border',
-          precision: 1,
-          showZero: true,
-          fontSize: 15,
-          fontColor: '#FFF',
-          fontStyle: 'bold',
-          fontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
-          showActualPercentages: true,
+          color: '#fff',
+          display: true,
+          // formatter: function (value, ctx) {
+          //   return ((value * 100) / total).toFixed(2) + '%';
+          // },
         },
       },
     },
